d44a105059156e20acdd10dfd09aaafdf5822181
KishMishNev <dkishechko@gmail.com>
2018-04-09 21:10:34 +0400

    Code refactoring: remove global variables, rename functions, added comments.

--- Code/Source.cpp
+++ Code/Source.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 typedef long long lint;
-
-lint A[10], B[10], C[10];
 
 lint gcd(lint a, lint b)
 {
@@ -12,39 +11,43 @@ lint gcd(lint a, lint b)
 	return gcd(b, a%b);
 }
 
-void multi(lint s1, lint s2)
+// Multiplication of polynomials
+vector<lint> multiplication(int s1, int s2, const vector<lint> &A, const vector<lint> &B)
 {
+	vector<lint> buf(s1 + s2 + 1, 0);
 	for (int i = 0; i <= s1 + s2; i++)
-		C[i] = 0;
-	for (lint i = 0; i <= s1 + s2; i++)
 	{
-		for (lint j = 0; j <= i; j++)
-			C[i] += A[j] * B[i - j];
+		for (int j = 0; j <= i; j++)
+			if (A.size() > j)
+				buf[i] += A[j] * B[i - j];
 	}
-	for (lint i = 0; i <= s1 + s2; i++)
-		A[i] = C[i];
+	return buf;
 }
 
-void derivative(lint n)
+// The derivative of a polynomial
+void derivative(int n, vector<lint> &A)
 {
-	for (lint i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 		A[i] = A[i + 1] * (i + 1);
 	A[n] = 0;
 }
 
-lint integral(lint n)
+//Calculation of integral and formula
+lint integral(int n, vector<lint> &A)
 {
 	lint pr = 1;
-	for (lint i = 2; i <= n + 1; i++)
+	A.resize(n + 2);
+	for (int i = 2; i <= n + 1; i++)
 		pr *= i;
-	for (lint i = n + 1; i > 0; i--)
+	for (int i = n + 1; i > 0; i--)
 	{
 		A[i] = A[i - 1] * (pr / i);
 	}
 	return pr;
 }
 
-lint mpow(lint x, lint y)
+// pow() for long long
+lint lintPow(lint x, lint y)
 {
 	lint a = 1;
 
@@ -55,34 +58,34 @@ lint mpow(lint x, lint y)
 
 int main()
 {
-	lint n;
+	int n;
 	lint numerator = 0, denominator = 1;
-	lint T[6];
 	cin >> n;
+	vector<lint> T(n + 1);
 	cin >> T[0];
 	lint max = T[0];
 	denominator *= T[0];
+	vector<lint> A(n + 1, 0), B(n + 1, 0);
 	A[0] = T[0];
 	A[1] = -1;
-	for (lint i = 1; i < n; i++)
+	for (int i = 1; i < n; i++)
 	{
 		cin >> T[i];
 		if (T[i] < max) max = T[i];
 		denominator *= T[i];
 		B[0] = T[i];
 		B[1] = -1;
-		multi(i, 1);
+		A = multiplication(i, 1, A, B);
 	}
 	for (int i = 0; i <= n; i++)
 		A[i] = -A[i];
-	derivative(n);
+	derivative(n, A);
 	for (int i = n; i>0; i--)
 		A[i] = A[i - 1];
 	A[0] = 0;
-	denominator *= integral(n);
-
+	denominator *= integral(n, A);
 	for (int i = 0; i <= n + 1; i++)
-		numerator += A[i] * (lint)mpow(max, i);
+		numerator += A[i] * lintPow(max, i);
 	lint g = gcd(numerator, denominator);
 	cout << numerator / g << "/" << denominator / g;
 	return 0;
